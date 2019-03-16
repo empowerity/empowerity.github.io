79d6d85c502558ab519f58b8690be9c2b2c6e825
lampk <phungkhanhlam@gmail.com>
2019-03-16 21:17:06 +0700

    update

---
+++ setup.R
@@ -0,0 +1,10 @@
+blogdown::new_site(theme = "yihui/hugo-xmag")
+## color:
+##- black:          0,0,0,100   #000000
+##- orange:         0,50,100,0  #ff8000
+##- sky blue:       80,0,0,0    #33ffff
+##- bluish green:   97,0,75,0   #08ff40
+##- yellow:         10,5,90,0   #e6f219
+##- blue:           100,50,0,0  #0080ff
+##- vermillion:     0,80,100,0  #ff3300
+##- reddish purple: 10,70,0,0   #e64dff
